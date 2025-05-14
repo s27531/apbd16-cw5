d8c6ce53d2c3bbeb5b13fdb7c656b3c6946db0e9
s27531 <s27531@pjwstk.edu.pl>
2025-05-14 15:00:54 +0200

    feat: Finalise DatabaseContext.cs

--- CodeFirst/Data/DatabaseContext.cs
+++ CodeFirst/Data/DatabaseContext.cs
@@ -1,9 +1,17 @@
+using CodeFirst.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CodeFirst.Data;
 
 public class DatabaseContext : DbContext
 {
+    
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Medicament> Medicaments { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Prescription> Prescriptions { get; set; }
+    public DbSet<PrescriptionMedicament> PrescriptionMedicaments { get; set; }
+    
     protected DatabaseContext()
     {
     }
@@ -14,7 +22,34 @@ public class DatabaseContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // TODO: Sample data.
-        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Doctor>().HasData(new List<Doctor>
+        {
+            new() { IdDoctor = 1, FirstName = "Jan", LastName = "Kowalski", Email = "jan.kowalski@nfz.gov.pl" },
+            new() { IdDoctor = 2, FirstName = "Andrzej", LastName = "Nowak", Email = "andrzej.nowak@nfz.gov.pl" }
+        });
+
+        modelBuilder.Entity<Patient>().HasData(new List<Patient>
+        {
+            new() { IdPatient = 1, FirstName = "Wojciech", LastName = "Nowak", Birthdate = DateOnly.Parse("1995-05-12") },
+            new() { IdPatient = 2, FirstName = "Mariusz", LastName = "Kowalski", Birthdate = DateOnly.Parse("1997-02-26") }
+        });
+
+        modelBuilder.Entity<Prescription>().HasData(new List<Prescription>
+        {
+            new() { IdPrescription = 1, Date = DateTime.Today, DueDate = DateTime.Now, IdPatient = 1, IdDoctor = 2 },
+            new() { IdPrescription = 2, Date = DateTime.Today, DueDate = DateTime.Now, IdPatient = 2, IdDoctor = 1 }
+        });
+
+        modelBuilder.Entity<Medicament>().HasData(new List<Medicament>
+        {
+            new() { IdMedicament = 1, Name = "Medicament Uno", Description = "Very stronk medicament", Type = "Type Uno" },
+            new() { IdMedicament = 2, Name = "Medicament Dos", Description = "Very weak medicament", Type = "Type Dos" },
+        });
+
+        modelBuilder.Entity<PrescriptionMedicament>().HasData(new List<PrescriptionMedicament>
+        {
+            new() { IdMedicament = 1, IdPrescription = 2, Dose = 2, Details = "Take doses in the morning." },
+            new() { IdMedicament = 2, IdPrescription = 1, Dose = 1, Details = "Take doses after dinner." },
+        });
     }
 }
